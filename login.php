0d6349ec9023c3f7b100da4f207ca7ed41b5be62
Émile Ménard-Croteau <emile.menard@outlook.com>
2019-05-02 12:42:14 -0400

    login

---
+++ login.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body style="background-color: rgb(244,245,247);">
+    <div>
+        <nav class="navbar navbar-light navbar-expand-md navigation-clean">
+            <div class="container">
+                <a class="navbar-brand" href="#">Galerie d'images</a>
+                <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse"
+                    id="navcol-1">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item" role="presentation"></li>
+                        <li class="nav-item" role="presentation"></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="subscribe.php">S'inscrire</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="container" style="height: 293px;margin: 200px auto;">
+        <div class="row">
+            <div class="col" style="margin: 0px 0px;">
+                <div class="card shadow" style="width: 420px;margin: auto;">
+                    <div class="card-body" style="margin: auto;width: 425px;padding: 40px;">
+                        <h4 class="card-title">Connexion</h4>
+                        <hr>
+                        <form action="./login_process.php" method="post">
+                            <div class="form-group">
+                                <label class="form-control-label" style="width: 100%;font-size: 16px;">Nom d'utilisateur</label>
+                                <input type="text" class="form-control" name="username">
+                            </div>
+                            <div class="form-group">
+                                <label class="form-control-label" style="width: 100%;font-size: 16px;">Mot de passe</label>
+                                <input class="form-control" name="password" type="password">
+                            </div>
+                            <?php
+                            if(isset($_GET['error'])) {
+                                if($_GET['error'] == true)
+                                    echo '<span style="color: red"> Username and password are invalid</span>';
+                            }
+                            ?>
+                            <button class="btn btn-primary float-right" style="margin: 10px 0px 0px 0px;" type="submit">
+                                Se connecter
+                            </button>
+                        </form>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ subscribe_process.php
@@ -0,0 +1,62 @@
+<?php 
+$location = './login.php';
+if(isset($_POST['username']) 
+&& isset($_POST['email']) 
+&& isset($_POST['firstname']) 
+&& isset($_POST['lastname'])
+&& isset($_POST['password'])
+&& isset($_POST['passwordconfirmation']) ){
+
+    $username= $_POST['username'];
+    $password= $_POST['email'];
+    $password= $_POST['firstname'];
+    $password= $_POST['lastname'];
+    $password= $_POST['password'];
+    $password= $_POST['passwordconfirmation'];
+
+    try
+    {
+
+        // À Remplacer
+	    $bd = new PDO('mysql:host=167.114.152.54;dbname=dbequipe14;charset=utf8', 'equipe14', '7klm98u8');
+        //
+       
+        $erreur = 0;
+        
+        $stm = $bd->prepare("CALL Authentification(?, ?)");
+
+        $stm->bindParam(1, $param_username);
+        $stm->bindParam(2, $param_password);
+
+        $param_username=$username;
+        $param_password=$password;
+
+        $reussi = $stm->execute();
+        $total = $stm->fetch();
+        
+        if($total[0] == 0) {
+            header('Location: '.$location . '?error=true');
+        }else{
+            session_start();
+            $_SESSION['username'] = $username;
+            echo "Connection reussi";
+        }
+
+    }
+ 
+    catch (PDOException $e)
+    {
+       echo('Erreur de connexion: ' . $e->getMessage());
+
+       exit();
+            
+    } 
+  
+
+
+
+}else {
+    header('Location: '.$location);
+}
+
+?>
